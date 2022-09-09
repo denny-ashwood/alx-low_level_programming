6c597e16498b6f64d9d80eedc95593dda483621a
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-09 05:38:04 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
@@ -30,7 +30,7 @@ int main(void)
 	}
 	else if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n, lastDigit);
 	}
 	return (0);
 }
