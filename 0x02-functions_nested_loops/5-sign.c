d871521147e793f68cd52de7e0c7884d7fa866a3
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-14 06:13:50 -0700

    Print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(n % 10 + '0');
+		_putchar(n % 10 + '0');
 
 		return (0);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 
 		return (-1);
 	}
