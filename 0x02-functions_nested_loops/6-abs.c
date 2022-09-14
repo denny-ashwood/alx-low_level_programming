defcca03faedc6b0abf0b2950b6a3a263f2e2be1
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-14 07:38:26 -0700

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @x: the integer passed to write absolute value
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int x)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
