a04584c6cd5c68786bebd99096dba2bd4322cfd1
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-14 07:44:15 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: number being tested
+ * Return: Always 0.
+ */
+
+int print_last_digit(int x)
+{
+	x = x % 10;
+
+	if (x < 0)
+	{
+		x = -x;
+
+		_putchar(x + '0');
+
+		return (x);
+	}
+}
