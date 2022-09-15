b516d6788c2baff66b634d460d4130028d653b08
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-15 14:31:30 -0700

    working

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
   * _isupper - Check if a letter is upper
@@ -6,15 +6,13 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
-
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }
+
