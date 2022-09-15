b23cf9fd496f9c737a1720e5946bbd85cc5f04ee
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-15 05:44:19 -0700

    updates

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "positive_or_negative.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -11,7 +10,7 @@ int main(void)
         int i;
 
         i = 98;
-        positive_or_negative(0);
+        positive_or_negative(i);
 
         return (0);
 }
