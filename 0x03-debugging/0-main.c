1be59a2c3c1b6e251f0951258d122b5078cc4285
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-15 05:23:11 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,34 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
+#include "positive_or_negative.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-}
-
 int main(void)
 {
         int i;
 
         i = 98;
-        positive_or_negative(i);
+        positive_or_negative(0);
 
         return (0);
 }
