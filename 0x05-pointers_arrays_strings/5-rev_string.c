9b0c0ea5b7f8cb887d94a182cfe2fb7b8b3074f7
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-19 08:36:48 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: passed variable
+ *
+ * Return: no return value
+ */
+
+void rev_string(char *s)
+{
+	int a, iterator, k;
+	char *a, tmp;
+
+	a = s;
+
+	while (s[iterator] != '\0')
+	{
+		iterator++;
+	}
+
+	for (k = 1; k < iterator; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (iterator / 2); i++)
+	{
+		tmp = s[i];
+		s[i] = *a;
+		*a = tmp;
+		a--;
+	}
+}
