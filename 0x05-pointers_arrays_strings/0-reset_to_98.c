a1e0e16817554701c110851d2aa65db6d9dd7b02
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-19 07:13:10 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * reset_to_98 - reset the value of the passed variable
+ *
+ * @*n: the passed variable to reset its value
+ *
+ * Return: no return value
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
