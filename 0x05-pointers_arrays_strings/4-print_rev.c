1769f039576878f82155f08dd86755a7cfbf123b
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-19 08:22:57 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: passed string variable to reverse its value
+ *
+ * Return: no return value
+ */
+
+void print_rev(char *s)
+{
+	while (*s != '\n')
+	{
+		_putchar(*s);
+
+		--s;
+	}
+	_putchar('\n');
+}
