6ea313dcc9cdef4411ea5ae68473cf1cef47fb94
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-22 14:44:33 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,32 @@
 #include "main.h"
-
 /**
- * _strcpy - See description
+ * _strcat - concatenates two strings
  *
- * @dest: char type string
- * @src: char type string
+ * @dest: the string @src is added to
+ * @src: the string to be appended
  *
- * Description: Copy the string pointed to by pointer 'src' to
- * the buffer pointed to by 'dest'
- *
- * Return: Pointer to `dest`
+ * Return: pointer to the resulting string @dest
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
+	int len1; /* string length for src */
+	int len2; /* string length for dest */
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
+	len1 = 0;
+	len2 = 0;
 
+	while (src[len1] != '\0')
+		len1++;
+
+	while (dest[len2] != '\0')
+		len2++;
+
+	for (i = 0; i <= len1; i++)
+	{
+		dest[len2] = src[i];
+		len2++;
+	}
 	return (dest);
 }
