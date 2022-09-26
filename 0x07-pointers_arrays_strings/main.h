aa8b7cfd8f606f678eb19499b8ffabddbd8209ed
Denzell S'fiso Daniel Yonah <denzell@sdnp.org.mw>
2022-09-26 13:50:40 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int coincidence(char *a, char *b);
-
+void print_chessboard(char (*a)[8]);
 
 #endif /* MAIN_H */
